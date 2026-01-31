32154f60ac88f7e93d4b1081c2bce09045e41d5c
M2DJ Gaming <m2dj2004@gmail.com>
2026-01-31 16:48:00 +0200

    Created the login and sign up pages and added there routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
       <AuthContextProvider>
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </AuthContextProvider>
     </>
   );
