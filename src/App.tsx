72ecbb0bcd8d889c2e1b7627c4c47c76a9aa8766
M2DJ Gaming <m2dj2004@gmail.com>
2026-02-02 13:42:52 +0200

    Removed the role choice page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { AuthContextProvider } from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
-import RoleChoicePage from "./pages/RoleChoice";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/role" element={<RoleChoicePage />} />
         </Routes>
       </AuthContextProvider>
     </>
