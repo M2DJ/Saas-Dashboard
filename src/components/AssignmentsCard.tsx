a66a35da747f1e6a4ba8976da7e9ee7a8b3756cf
M2DJ Gaming <m2dj2004@gmail.com>
2026-02-05 17:35:25 +0200

    Finished the Assignment card

--- src/components/AssignmentsCard.tsx
+++ src/components/AssignmentsCard.tsx
@@ -1,8 +1,32 @@
+import { format } from "date-fns";
 
-const AssignmentsCard = () => {
+type AssignmentCardProps = {
+  className: string;
+  dueDate: Date;
+};
+
+const AssignmentsCard = ({ className, dueDate }: AssignmentCardProps) => {
+  const clipClassName = (nameOfClass: string) => {
+    if (nameOfClass.includes(" ")) {
+      return nameOfClass.split(" ")[1].charAt(0);
+    }
+    return nameOfClass.charAt(0);
+  };
+
   return (
-    <div>AssignmentsCard</div>
-  )
-}
+    <div className="h-[110px] rounded-xl flex items-center px-3 shadow-[0px_0px_10px_rgba(0,0,0,0.3)]">
+      <div className="h-22 w-22 bg-[#707070] rounded-xl flex justify-center items-center mr-2">
+        <p className="text-5xl text-white">{clipClassName(className)}</p>
+      </div>
+      <div>
+        <p className="text-xl mb-3">{className}</p>
+        <p className="text-[#7D7D7D]">
+          Due
+          <span className="italic">{format(dueDate, "MMM do, h:mm a")}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default AssignmentsCard
+export default AssignmentsCard;
--- src/pages/AssignmentsPage.tsx
+++ src/pages/AssignmentsPage.tsx
@@ -1,9 +1,22 @@
-
+import AssignmentsCard from "../components/AssignmentsCard";
 
 const AssignmentsPage = () => {
-  return (
-    <div>AssignmentsPage</div>
-  )
-}
+  const assignments = [{ className: "Python", dueDate: new Date() }];
 
-export default AssignmentsPage
+  return (
+    <div className="font-inter">
+      <div className="h-20 py-3 px-6">
+        <p className="text-4xl mb-2">Assignments</p>
+        <div className="h-[0.1px] bg-[#B2A9A9]"></div>
+      </div>
+
+      <div className="px-6">
+        {assignments.map((assignment) => (
+          <AssignmentsCard className={assignment.className} dueDate={assignment.dueDate} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AssignmentsPage;
