066845805aaf076e6f4e21a9ebeb3422db60d336
M2DJ Gaming <m2dj2004@gmail.com>
2026-02-11 16:50:48 +0200

    Added the contents page for the assignments

---
+++ src/pages/AssignmentContent.tsx
@@ -0,0 +1,48 @@
+import Arrow_Back from "../assets/images/Arrow_Back.webp";
+
+type AssignmentProps = {
+  nameOfClass: string;
+  assignmentTitle: string;
+  desc: string;
+  file: string;
+  onClick: () => void;
+};
+
+const AssignmentContent = ({
+  nameOfClass,
+  assignmentTitle,
+  desc,
+  file,
+  onClick,
+}: AssignmentProps) => {
+  return (
+    <div className="font-inter">
+      <div className="h-20 py-3 px-6">
+        <div className="mb-2 flex items-center">
+          <button onClick={onClick} className="mr-3">
+            <img src={Arrow_Back} className="w-[34px]" />
+          </button>
+          <p className="text-4xl">
+            <span className="text-gray-600 opacity-50">Assignments</span> /{" "}
+            {nameOfClass}
+          </p>
+        </div>
+        <div className="h-[0.1px] bg-[#B2A9A9]"></div>
+
+        <div className="w-100 mt-3">
+          <p className="text-3xl font-bold mb-2">{assignmentTitle}</p>
+
+          <p className="mb-12">{desc}</p>
+
+          <p className="mb-2">{file}</p>
+
+          <button className="rounded-md py-2 px-1 text-lg text-white bg-[#5B92FF] shadow-[0px_2px_10px_rgba(0,0,0,0.4)]">
+            Upload files
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AssignmentContent;
--- src/pages/AssignmentsPage.tsx
+++ src/pages/AssignmentsPage.tsx
@@ -1,21 +1,44 @@
+import { useState } from "react";
 import AssignmentsCard from "../components/AssignmentsCard";
+import AssignmentContent from "./AssignmentContent";
 
 const AssignmentsPage = () => {
+  const [selectedAssignment, setSelectedAssignment] = useState(false);
+
   const assignments = [{ className: "Python", dueDate: new Date() }];
 
   return (
-    <div className="font-inter">
-      <div className="h-20 py-3 px-6">
-        <p className="text-4xl mb-2">Assignments</p>
-        <div className="h-[0.1px] bg-[#B2A9A9]"></div>
-      </div>
+    <>
+      {selectedAssignment ? (
+        <AssignmentContent
+          nameOfClass={"Python"}
+          desc={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
+            labore ab cupiditate sit mollitia culpa et, in illum voluptates
+            voluptatum molestiae accusantium nostrum! Consectetur vero incidunt
+            ratione autem vitae facere impedit, fugiat esse cumque! Delectus
+            aperiam quis autem minima voluptate, blanditiis tempora expedita
+            excepturi dolores maiores. Repellat quos ipsam beatae.`}
+          file={"File URL Here"}
+          onClick={() => setSelectedAssignment(false)} 
+          assignmentTitle={"First Assignment"}        />
+      ) : (
+        <div className="font-inter">
+          <div className="h-20 py-3 px-6">
+            <p className="text-4xl mb-2">Assignments</p>
+            <div className="h-[0.1px] bg-[#B2A9A9]"></div>
+          </div>
 
-      <div className="px-6">
-        {assignments.map((assignment) => (
-          <AssignmentsCard className={assignment.className} dueDate={assignment.dueDate} />
-        ))}
-      </div>
-    </div>
+          <div onClick={() => setSelectedAssignment(true)} className="px-6">
+            {assignments.map((assignment) => (
+              <AssignmentsCard
+                className={assignment.className}
+                dueDate={assignment.dueDate}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
