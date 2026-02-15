e3c94158f2e77eb2efd47c95b30a8a29dcedbcca
M2DJ Gaming <m2dj2004@gmail.com>
2026-02-15 19:28:47 +0200

    Added the context of the table context provider to the App file

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/Dashboard";
+import { TableContextProvider } from "./context/TableContext";
 
 function App() {
   return (
     <>
       <AuthContextProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />}/>
-        </Routes>
+        <TableContextProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+        </TableContextProvider>
       </AuthContextProvider>
     </>
   );
